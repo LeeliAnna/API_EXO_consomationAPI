ca98573e0790873ced9380c8c2f8acaf803f5605
LeeliAnna <gaelle.timmermans@gmail.com>
2024-01-12 15:37:51 +0100

    modif

--- DAL/Repositories/UserRepository.cs
+++ DAL/Repositories/UserRepository.cs
@@ -7,55 +7,84 @@ namespace DAL.Repositories
     public class UserRepository : IUserRepository
     {
 
-        public User Create(User user)
+        //public User Create(User user)
+        //{
+        //    int Id = FakeDB.Users.Max(x => x.Id) + 1;
+
+        //    user.Id = Id;
+
+        //    FakeDB.Users.Add(user);
+
+        //    return user;
+        //}
+
+        //public User? GetByEmail(string email)
+        //{
+        //    User? u = FakeDB.Users.SingleOrDefault(x => x.Email == email);
+
+        //    return u;
+        //}
+
+        //public User? GetById(int id)
+        //{
+        //    User? u = FakeDB.Users.Find(x => x.Id == id);
+
+        //    return u;
+        //}
+
+        //public IEnumerable<User> GetAll()
+        //{
+        //    return FakeDB.Users;
+        //}
+
+        //public bool Update(User user)
+        //{
+        //    User? u = FakeDB.Users.Find(x => x.Id == user.Id);
+
+        //    if (u != null)
+        //    {
+        //        u.Pseudo = user.Pseudo;
+        //        u.Email = user.Email;
+        //        u.Password = user.Password;
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+
+        //public bool Delete(User user)
+        //{
+        //    FakeDB.Users.Remove(user);
+        //    return true;
+        //}
+        public Task Create(User user)
         {
-            int Id = FakeDB.Users.Max(x => x.Id) + 1;
-
-            user.Id = Id;
-
-            FakeDB.Users.Add(user);
-
-            return user;
+            throw new NotImplementedException();
         }
 
-        public User? GetByEmail(string email)
+        public Task<bool> Delete(User user)
         {
-            User? u = FakeDB.Users.SingleOrDefault(x => x.Email == email);
-
-            return u;
+            throw new NotImplementedException();
         }
 
-        public User? GetById(int id)
+        public Task GetAll()
         {
-            User? u = FakeDB.Users.Find(x => x.Id == id);
-
-            return u;
+            throw new NotImplementedException();
         }
 
-        public IEnumerable<User> GetAll()
+        public Task? GetByEmail(string email)
         {
-            return FakeDB.Users;
+            throw new NotImplementedException();
         }
 
-        public bool Update(User user)
+        public Task? GetById(int id)
         {
-            User? u = FakeDB.Users.Find(x => x.Id == user.Id);
-
-            if (u != null)
-            {
-                u.Pseudo = user.Pseudo;
-                u.Email = user.Email;
-                u.Password = user.Password;
-                return true;
-            }
-
-            return false;
+            throw new NotImplementedException();
         }
 
-        public bool Delete(User user)
+        public Task<bool> Update(User user)
         {
-            FakeDB.Users.Remove(user);
-            return true;
+            throw new NotImplementedException();
         }
     }
 }
